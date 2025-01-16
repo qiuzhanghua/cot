60b9437bcd379a76e0e3014d71685015861c23c8
qiuzhanghua <qiuzhanghua@icloud.com>
2025-01-16 22:30:13 +0800

    feat: add log4rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
 use clap::{command, Arg, ArgAction, Command};
+use log::info;
+use log4rs::{self, config::RawConfig};
 use rust_embed::Embed;
+use std::env;
 
 #[derive(Embed)]
 #[folder = "static/"]
-#[prefix = "static/"]
+#[prefix = ""]
 struct Asset;
 
 fn main() {
-    for file in Asset::iter() {
-        println!("{}", file.as_ref());
+    // for file in Asset::iter() {
+    //     println!("{}", file.as_ref());
+    // }
+    // use current directory log4rs.yml if exists
+    let mut init_log = log4rs::init_file("log4rs.yml", Default::default());
+    // otherwise use log4rs.yaml in same directory as executable
+    if init_log.is_err() {
+        if let Ok(exe_path) = env::current_exe() {
+            let exe_dir = exe_path.parent().unwrap();
+            let log4rs_yml = exe_dir.join("log4rs.yml");
+            init_log = log4rs::init_file(log4rs_yml, Default::default());
+        }
     }
+    if init_log.is_err() {
+        let log4rs_yaml = Asset::get("log4rs.yaml").unwrap();
+        let log4rs_yaml_str = std::str::from_utf8(log4rs_yaml.data.as_ref()).unwrap();
+        let config: RawConfig = serde_yaml::from_str(log4rs_yaml_str).unwrap();
+        log4rs::init_raw_config(config).unwrap();
+    }
+    // set logging level to off default
+    // if LOGGING_LEVEL is set in environment, use that
+    let logging_level = env::var("LOGGING_LEVEL").unwrap_or("off".to_string());
+    let logging_level = match logging_level.to_lowercase().as_str() {
+        "trace" => log::LevelFilter::Trace,
+        "debug" => log::LevelFilter::Debug,
+        "info" => log::LevelFilter::Info,
+        "warn" => log::LevelFilter::Warn,
+        "error" => log::LevelFilter::Error,
+        _ => log::LevelFilter::Off,
+    };
+    log::set_max_level(logging_level);
+
+    info!("Hello, log4rs!");
 
     let _matches = command!()
         .help_template(
